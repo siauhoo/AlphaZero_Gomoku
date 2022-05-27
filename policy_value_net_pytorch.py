4642e64578d7b38a604cfd57f1d8067342f4c442
siauhoo <siauhoo@gmail.com>
2022-05-27 16:32:19 +0800

    bug fix

--- policy_value_net_pytorch.py
+++ policy_value_net_pytorch.py
@@ -145,9 +145,9 @@ class PolicyValueNet():
         entropy = -torch.mean(
                 torch.sum(torch.exp(log_act_probs) * log_act_probs, 1)
                 )
-        return loss.data[0], entropy.data[0]
+        #return loss.data[0], entropy.data[0]
         #for pytorch version >= 0.5 please use the following line instead.
-        #return loss.item(), entropy.item()
+        return loss.item(), entropy.item()
 
     def get_policy_param(self):
         net_params = self.policy_value_net.state_dict()
